cf0f161dac820776d6825fc6e853e3a3a1e64934
Kunal Patil <kunalpatil970730@gmail.com>
2023-01-07 16:48:28 +0530

    commit

--- project/views.py
+++ project/views.py
@@ -4,15 +4,20 @@ from newscatcherapi import NewsCatcherApiClient
 
 def index(request):
 
-    newscatcherapi = NewsCatcherApiClient(x_api_key='j9eDrL_PjAatxMYJu9Tox0xANBQYmGePDDArY7aO4mQ')
+    try:
+        newscatcherapi = NewsCatcherApiClient(x_api_key='j9eDrL_PjAatxMYJu9Tox0xANBQYmGePDDArY7aO4mQ')
 
-    all_articles = newscatcherapi.get_search(q='Elon Musk',
-                                            lang='en',
-                                            countries='CA',
-                                            page_size=100)
-    print(all_articles['articles'])
+        all_articles = newscatcherapi.get_search(q='Elon Musk',
+                                                lang='en',
+                                                countries='CA',
+                                                page_size=100)
+        print(all_articles['articles'])
 
-    context = {
-        "all_articles" : all_articles['articles'][:2],
-    }
-    return render(request, 'index.html', context)   
+        context = {
+            "all_articles" : all_articles['articles'][:2],
+        }
+        return render(request, 'index.html', context)   
+    except:
+        pass
+
+    return render(request, 'index.html')   
