20f856e5ffbd8103ed69a4ab36af92c7b982fcec
Kunal Patil <kunalpatil970730@gmail.com>
2023-01-07 18:26:38 +0530

    commit

--- app/urls.py
+++ app/urls.py
@@ -18,5 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("other/", views.other, name="other")
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from newscatcherapi import NewsCatcherApiClient
-
+import requests
 
 def index(request):
-
     try:
         newscatcherapi = NewsCatcherApiClient(x_api_key='j9eDrL_PjAatxMYJu9Tox0xANBQYmGePDDArY7aO4mQ')
 
@@ -21,3 +20,26 @@ def index(request):
         pass
 
     return render(request, 'index.html')   
+
+
+
+def other(request):
+    url = 'https://api.worldnewsapi.com/search-news'
+    parameters = {
+    'text': 'Bamboo Development', # query phrase
+    'sort': 'publish-time',  # maximum is 100
+    'api-key': 'aaa245439f6e4d2093fa3e4235bb8459',  # your own API key
+    'source-countries' : 'in'
+}
+    response = requests.get(url, params=parameters)
+    news_list = response.json()
+    try:
+        context = {
+            'news_list' : list(news_list['news'][:2]),
+        }
+        return render(request, 'news.html', context)
+    except:
+        pass
+       
+    return render(request, 'news.html')
+
